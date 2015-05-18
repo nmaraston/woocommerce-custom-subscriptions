afcd3dd269e967fd959af34329ae427f06de374a
nmaraston <nick.maraston@gmail.com>
2015-05-18 14:56:17 -0400

    Implement script for enabling subscription add/edit fields for a Custom Subscription product type

---
+++ assets/js/wccs_product_creation_form.js
@@ -0,0 +1,30 @@
+/**
+ * This script adds support for the existing add/edit product form fields - provided by the WooCommerce
+ * Subscriptions plugin - to be displayed when editing a Custom Subscription product type.
+ */
+
+jQuery(document).ready(function($){
+
+	$.extend({
+		showHideCustomSubscriptionMeta: function(){
+			if ($('select#product-type').val()=='custom-subscription') {
+				$('.show_if_simple').show();
+				$('.show_if_subscription').show();
+				$('.grouping_options').hide();
+				$('.options_group.pricing ._regular_price_field').hide();
+				$('#sale-price-period').show();
+			} else {
+				$('.options_group.pricing ._regular_price_field').show();
+				$('#sale-price-period').hide();
+			}
+		}
+	});
+
+	if($('.options_group.pricing').length > 0) {
+		$.showHideCustomSubscriptionMeta();
+	}
+
+	$('body').bind('woocommerce-product-type-change',function(){
+		$.showHideCustomSubscriptionMeta();
+	});
+});
