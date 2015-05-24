fc4bc44cc05a381522ea925cbfd69639e8838602
nmaraston <nick.maraston@gmail.com>
2015-05-24 02:11:35 -0400

    Implement WCCS_UISM_State to contain possible UISM states

---
+++ includes/class-wccs-uism-state.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This class contains possible states of a UISM.
+ *
+ * @class       WCCS_UISM_State
+ * @package     WooCommerceCustomSubscriptions
+ * @category    Class
+ * @version     1.0
+ * @since       1.0
+ * @author      Nick Maraston
+ */
+class WCCS_UISM_State {
+
+	public static $ACTIVE_NONBILLING = "ACTIVE_NONBILLING";
+	public static $ACTIVE_BILLING    = "ACTIVE_BILLING";
+	public static $INACTIVE          = "INACTIVE";
+}
