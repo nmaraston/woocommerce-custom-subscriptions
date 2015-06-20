d72132c3e9b7cf53c8b430c02335c47fb667fac3
nmaraston <nick.maraston@gmail.com>
2015-06-19 23:14:01 -0400

    Load subscription selection page template on subscription selection page_id load

--- includes/configuration/class-wccs-template-loader.php
+++ includes/configuration/class-wccs-template-loader.php
@@ -15,30 +15,44 @@ class WCCS_Template_Loader {
 	 *
 	 * Here we override template loading to load our page templates.
 	 *
-	 * As in WC, templates are in the "templates" folder and theme overrides
-	 * are used if provided in the /theme/woocommerce/ directory.
+	 * Similar to WooCommerce, templates are in the plugin's "templates" folder
+	 * and theme overrides are used if provided in the
+	 * /theme/woocommerce/custom-subscriptions directory.
 	 *
 	 * @param mixed $template
 	 * @since 1.0
 	 * @return string
 	 */
 	public static function fh_template_include( $template ) {
-		$find = array( "woocommerce-custom-subscriptions.php" );
+		$find = array( 'woocommerce-custom-subscriptions.php' );
 		$file = "";
 		$allow_override = true;
 
-		if ( is_page( WCCS_Page_Configuration::get_page_id( "mysubscription" ) ) ) {
+		if ( is_page( WCCS_Page_Configuration::get_page_id( 'mysubscription' ) ) ) {
+			$file = 'my-subscription/my-subscription.php';
+
+			$find[] = $file;
 
 			// Do not allow template overriding for the top level my-subscription template file.
 			$allow_override = false;
-			$file = "my-subscription.php";
-			$template = WCCS()->default_template_path() . 'my-subscription/' . $file;
+
 			WCCS_Asset_Loader::enqueue_wccs_my_subscription_styles();
+		} elseif ( is_page( WCCS_Page_Configuration::get_page_id( 'subscriptionselection' ) ) ) {
+			$file = "pages/subscription-selection.php";
+
+			$find[] = $file;
+			$find[] = WCCS()->override_template_path() . $file;
+
+			$allow_override = true;
 		}
 
-		if ( $file && $allow_override ) {
-			$template = locate_template( array_unique( $find ) );
-			if ( ! $template ) {
+		if ( $file ) {
+			if ( $allow_override ) {
+				$template = locate_template( array_unique( $find ) );
+				if ( ! $template ) {
+					$template = WCCS()->default_template_path() . $file;
+				}
+			} else {
 				$template = WCCS()->default_template_path() . $file;
 			}
 		}
